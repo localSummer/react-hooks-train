821c2ad40d1b4414e457f998b4d0186b079680ac
wangxingwen <wangxingwen@liepin.com>
2021-03-05 20:16:39 +0800

    feat: 组件懒加载

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,13 +3,15 @@ import { HashRouter, Switch, Route, Redirect } from 'react-router-dom'
 import loadable from '@loadable/component'
 
 const Todos = loadable(() => import(/* webpackChunkName: "Todos" */ '../views/Todos'))
+const Hooks = loadable(() => import(/* webpackChunkName: "Hooks" */ '../views/Hooks'))
 
 const Router = () => {
   return (
     <HashRouter>
       <Switch>
         <Route exact path="/todo" component={Todos} />
-        <Redirect path="*" to="/todo" />
+        <Route exact path="/hooks" component={Hooks} />
+        <Redirect path="*" to="/hooks" />
       </Switch>
     </HashRouter>
   )
