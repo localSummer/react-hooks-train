db587f657cb648d7bf79a495e1d0ad200924fe23
localsummer <roamingcode@163.com>
2021-07-24 15:51:16 +0800

    feat: update

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,15 @@
-const myInstanceof = (left, right) => {
-  // 基础类型直接退出
-  if (typeof left !== 'object' || left === null) return false
-  let proto = Object.getPrototypeOf(left)
-  while (true) {
-    if (proto === null) return false
-    if (proto === right.prototype) {
-      return true
+function defineReactive(data, key, val) {
+  Object.defineProperty(data, key, {
+    enumerable: true,
+    configurable: true,
+    get() {
+      console.log('get: ', val);
+      return val;
+    },
+    set(newVal) {
+      console.log('set: ', newVal);
+      if (val === newVal) return;
+      val = newVal;
     }
-    proto = Object.getPrototypeOf(proto)
-  }
-}
-
-const getType = (obj) => {
-  let type = typeof obj
-  // 基础类型直接返回，function类型也返回
-  if (type !== 'object' || obj === null) return type
-  return console.log(Object.prototype.toString.call(obj).replace(/^\[object (\S+)\]$/, '$1'))
+  })
 }
